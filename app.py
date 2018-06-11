bf3aafb439f2c6f4fdf5bf3aaaf0a1aa4ae9a96c
Alex Brassel <alexbrassel2020@gmail.com>
2018-06-11 17:33:01 -0400

    test

--- app.py
+++ app.py
@@ -9,11 +9,8 @@ manager = message_loader.Manager(TOKEN)
 
 @app.route('/', methods=['POST'])
 def index():
-    if request.json:
-        print(request.get_json())
-        print(manager.group_list)
-        print(request.json['group_id'])
-    manager.send_message('this is a test response',request.json['group_id'])
+    if request.json and request.json['name'] != 'Brassel Sprouts':
+        manager.send_message('this is a test response',request.json['group_id'])
 
 
 if __name__ == '__main__':
