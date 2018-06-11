042933369bfda2b61339506e3d734862b4790a58
Alex Brassel <alexbrassel2020@gmail.com>
2018-06-11 17:17:30 -0400

    from echo and parsing features

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
+import message_loader
+from message_loader import TOKEN
+
 
 app = Flask(__name__)
+manager = message_loader.Manager(TOKEN)
 
 @app.route('/', methods=['POST'])
 def index():
     if request.json:
         print(request.get_json())
-    return "Hello World"
+        print(manager.group_list)
+    manager.send_message('this is a test response')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print('ran')
