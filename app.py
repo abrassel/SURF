661dacf8b706f86c208336b8668500faefef4ae7
Alex Brassel <abrassel@umd.edu>
2018-06-12 00:47:33 -0400

    fixed

--- app.py
+++ app.py
@@ -13,14 +13,10 @@ def index():
 
         if "groups" in request.json['text']:
             groups = ""
-            for group in manager.myself.groups.list_all():
+            for group in manager.group_list.values():
                 groups += group.name + "\n"
+            manager.msg_bot(groups)
 
-            ind = 0
-            for ind in range(0,len(groups),100):
-                manager.msg_bot(groups[ind:ind+100])
-            if ind < len(groups):
-                manager.msg_bot(groups[ind:])
     return '200'
 
 
