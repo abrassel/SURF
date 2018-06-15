1d0022896fcd1d17c386241bde79c8de31aac3fc
Alex Brassel <alexbrassel2020@gmail.com>
2018-06-15 09:47:02 -0400

    test

--- app.py
+++ app.py
@@ -70,7 +70,9 @@ def webhook():
     - unhook: remove bot from channel
     - privilege <channel name> <admin/all>: if admin, restrict bot usage to admin/all
     '''
-    print(manager.privileged[chat_id])
+
+    print(manager.is_owner(sender_id, chat_id))
+
     if manager.privileged[chat_id] and not manager.is_owner(sender_id, chat_id):
         print('failed privilege check')
         return '200'
