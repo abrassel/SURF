111adb715a6583acf24b7e121fc48934b3524949
Brassel <asb7@NIST.GOV>
2018-06-26 16:04:49 -0400

    test

--- API.py
+++ API.py
@@ -1,23 +1,31 @@
 import requests
+import json
+from time import time
+
+SEND_URL = '/direct_messages'
+token = 'Tb4nzBPaxX9YXzw382Zm3g7jb0MFYPRzLAWl2FeIq'
+
+headers = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'X-Access-Token': 'Tb4nzBPaxX9YXzw382Zm3g7jb0MFYPRzLAWl2FeI',
+}
+
 
 class API:
-    def __init__(self, token):
-        self._base = "https://api.groupme.com/v3"
-        self._headers = {"Content-Type": "applications/json"}
-        self._params = {"token": token}
-
+    def __init__(self):
+        self.base = 'https://api.groupme.com/v3'
 
     def send_msg(self, user_id, msg):
-        data = {'source_guid': 'GUID',
-                'recipient_id': user_id,
-                'text': msg
-                }
+        data = {"direct_message":
+        {
+            "text": msg,
+            "attachments": [],
+            "recipient_id": user_id,
+            "source_guid": str(time())}
+        }
 
-        self.__post__(extension="/direct_messages",data=data)
-        
+        data = json.dumps(data, indent=2)
 
-    def __post__(self,extension,data):
-        r = requests.post(self._base + extension, headers=self._headers,
-                             params=self._params, data=data)
-        print(r.__dict__)
-        return r
+        return requests.post(self.base + SEND_URL,
+                      headers=headers,
+                      data=str(data))
