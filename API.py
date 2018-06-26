d94dd5b142cb72c56cbab6dfac1300d351ec32aa
Alex Brassel <alexbrassel2020@gmail.com>
2018-06-26 16:16:35 -0400

    test

--- API.py
+++ API.py
@@ -1,19 +1,20 @@
 import requests
 import json
 from time import time
+import os
 
 SEND_URL = '/direct_messages'
 token = 'Tb4nzBPaxX9YXzw382Zm3g7jb0MFYPRzLAWl2FeIq'
 
 headers = {
     'Content-Type': 'application/json;charset=UTF-8',
-    'X-Access-Token': 'Tb4nzBPaxX9YXzw382Zm3g7jb0MFYPRzLAWl2FeI',
+    'X-Access-Token': os.environ.get('token'),
 }
 
+base = 'https://api.groupme.com/v3'
 
 class API:
-    def __init__(self, token):
-        self.base = 'https://api.groupme.com/v3'
+    def __init__(self):
 
     def send_msg(self, user_id, msg):
         data = {"direct_message":
@@ -26,6 +27,6 @@ class API:
 
         data = json.dumps(data, indent=2)
 
-        print(requests.post(self.base + SEND_URL,
+        print(requests.post(base + SEND_URL,
                       headers=headers,
                       data=str(data)).__dict__)
